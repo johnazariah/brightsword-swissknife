7728805d9939ff16cb973ee66d11cd34d3390842
John Azariah <john.azariah@gmail.com>
2016-03-03 04:58:17 -0800

    Fix up project name in document generator

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -11,7 +11,7 @@
 // (This is the original behaviour of ProjectScaffold prior to multi project support)
 let referenceBinaries = []
 // Web site location for the generated documentation
-let website = "/BrightSword.SwissKnife"
+let website = "/brightsword-swissknife"
 
 let githubLink = "http://github.com/johnazariah/brightsword-swissknife"
 
